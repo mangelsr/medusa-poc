1ab9d469eade1534eae4f72de461b803d2fae52d
Miguel Sanchez <mangelsr25@gmail.com>
2026-03-28 12:01:55 -0500

    [ADD] Configs to work

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -11,6 +11,28 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+    },
+    databaseDriverOptions: {
+      ssl: false,
+      sslmode: "disable",
+    },
+  },
+  admin: {
+    vite: (config) => {
+      return {
+        server: {
+          host: "0.0.0.0",
+          // Allow all hosts when running in Docker (development mode)
+          // In production, this should be more restrictive
+          allowedHosts: ["localhost", ".localhost", "127.0.0.1"],
+          hmr: {
+            // HMR websocket port inside container
+            port: 5173,
+            // Port browser connects to (exposed in docker-compose.yml)
+            clientPort: 5173,
+          },
+        },
+      };
+    },
+  },
+});
